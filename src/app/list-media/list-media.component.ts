4f7037d6e11d0551c710a80c448ed1c184a589a7
Henri Pallasaho <henripal@ety-a2123b-26.edu.wks.metropolia.fi>
2018-01-23 12:30:38 +0200

    second time?

--- src/app/list-media/list-media.component.ts
+++ src/app/list-media/list-media.component.ts
@@ -19,11 +19,21 @@ export class ListMediaComponent implements OnInit {
         this.tervehdys = this.mediaService.testi;
         this.mediaService.getAllMedia().subscribe(data => {
             this.kaikkiMedia = data;
+            console.log(this.kaikkiMedia);
 
-            const temp = this.kaikkiMedia[0].filename.split('.');
-            console.log(temp);
-            const uusinimi = temp[0] + '-tn320.png';
-            console.log(uusinimi);
+/*
+            for(let i = 0; i < this.kaikkiMedia.length; i++){
+                const temp = this.kaikkiMedia[1].filename.split('.');
+                const uusinimi = temp[0] + '-tn320.png';
+                this.kaikkiMedia[i].thumbnail = uusinimi;
+            }
+
+*/
+            this.kaikkiMedia.map(media => {
+                const temp = media.filename.split('.');
+                const uusinimi = temp[0] + '-tn320.png';
+                media.thumbnail = uusinimi;
+            });
 
 
             console.log(this.kaikkiMedia);
